f12434e15e13a9433c5b096bd527a563e18ba6c0
GaetanoNesea <g.sabino@nesea.it>
2021-12-30 08:19:30 +0100

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import './style.css';
 
 import { of, map, Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 of('World')
   .pipe(map((name) => `Hello, ${name}!`))
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+
+ajax.get
